d7684ebda0808b2811fb85c8ed51dd842f7c776a
Jerry Hon <hon.jerry@gmail.com>
2022-08-19 06:25:10 +0200

    refactor(app): move static values to config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import DataTable from './DataTable';
 import './App.css';
+import { LOCALE, ROWS_PER_PAGE } from './config';
 
 function App(props) {
   return (
     <div className="container mt-3">
-      <DataTable rows={props.rows} locale="da" rowsPerPage={5} />
+      <DataTable rows={props.rows} locale={LOCALE} rowsPerPage={ROWS_PER_PAGE} />
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const LOCALE = 'da';
+export const ROWS_PER_PAGE = 5;
